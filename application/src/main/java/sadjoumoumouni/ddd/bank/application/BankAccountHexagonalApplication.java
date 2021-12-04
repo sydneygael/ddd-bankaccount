901b20f98c045157896c42925ed08500aa86aec4
Sydney Adjou-Moumouni <Sydney.ADJOU-MOUMOUNI@cpexterne.org>
2021-12-04 16:58:39 +0100

    Add data to use app

--- application/src/main/java/sadjoumoumouni/ddd/bank/application/BankAccountHexagonalApplication.java
+++ application/src/main/java/sadjoumoumouni/ddd/bank/application/BankAccountHexagonalApplication.java
@@ -1,10 +1,17 @@
 package sadjoumoumouni.ddd.bank.application;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import sadjoumoumouni.ddd.bank.domain.BankAccount;
+import sadjoumoumouni.ddd.bank.infrastructure.adapters.outgoing.inmemory.InMemoryBankAccountRepository;
 
 @SpringBootApplication (scanBasePackages = {"sadjoumoumouni.ddd.bank.infrastructure.adapters",
         "sadjoumoumouni.ddd.bank.application"},
@@ -15,5 +22,14 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 public class BankAccountHexagonalApplication {
     public static void main(final String[] args) {
         SpringApplication.run(BankAccountHexagonalApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner bootstrapData(InMemoryBankAccountRepository repository) {
+        return (args) -> {
+            BigDecimal initialBalance = BigDecimal.valueOf(100);
+            BankAccount bankAccount = new BankAccount(1L, initialBalance);
+            repository.save(bankAccount);
+        };
     }
 }
