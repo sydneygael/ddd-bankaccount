39362432b49d62b8dbfcd4b93149c50d9bf3787d
Sydney Adjou-Moumouni <Sydney.ADJOU-MOUMOUNI@cpexterne.org>
2021-12-04 15:24:25 +0100

    Infra part 2

--- infrastructure/src/main/java/sadjoumoumouni/ddd/bank/infrastructure/adapters/configuration/BeanConfiguration.java
+++ infrastructure/src/main/java/sadjoumoumouni/ddd/bank/infrastructure/adapters/configuration/BeanConfiguration.java
@@ -1,15 +1,18 @@
 package sadjoumoumouni.ddd.bank.infrastructure.adapters.configuration;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import sadjoumoumouni.ddd.bank.domain.services.BankAccountDomainService;
-import sadjoumoumouni.ddd.bank.infrastructure.adapters.outgoing.jpa.JpaBankAccountRepository;
+import sadjoumoumouni.ddd.bank.infrastructure.adapters.outgoing.inmemory.InMemoryBankAccountRepository;
 
 @Configuration
+@ComponentScan(basePackages = "sadjoumoumouni.ddd.bank.infrastructure.adapters")
 public class BeanConfiguration {
 
     @Bean
-    BankAccountDomainService bankAccountService(JpaBankAccountRepository repository) {
+    BankAccountDomainService bankAccountService(InMemoryBankAccountRepository repository) {
         return new BankAccountDomainService(repository);
     }
 }
--- infrastructure/src/main/java/sadjoumoumouni/ddd/bank/infrastructure/adapters/outgoing/inmemory/InMemoryBankAccountRepository.java
+++ infrastructure/src/main/java/sadjoumoumouni/ddd/bank/infrastructure/adapters/outgoing/inmemory/InMemoryBankAccountRepository.java
@@ -1,6 +1,8 @@
 package sadjoumoumouni.ddd.bank.infrastructure.adapters.outgoing.inmemory;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Component;
 
@@ -11,9 +13,11 @@ import sadjoumoumouni.ddd.bank.domain.ports.outgoing.AccountPort;
 @Component
 public class InMemoryBankAccountRepository implements AccountPort {
 
+    private Map<String, BankAccount> inMemoryBankDb = new ConcurrentHashMap<>();
+
     @Override
     public void save(BankAccount bankAccount) {
-
+        inMemoryBankDb.put(bankAccount.getAccountId().toString(),bankAccount);
     }
 
     @Override
@@ -23,6 +27,6 @@ public class InMemoryBankAccountRepository implements AccountPort {
 
     @Override
     public Optional<BankAccount> loadAccountById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(inMemoryBankDb.get(id.toString()));
     }
 }
